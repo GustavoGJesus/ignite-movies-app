35e380ddab45d05d546c600deea8a6fd1fd0a4f6
GustavoGJesus <icarogabriel01@protonmail.com>
2021-08-28 16:19:54 -0300

    Sidebar finish

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,32 @@
-import {useState, useEffect} from 'react';
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+
 import '../styles/sidebar.scss'
 
-export function SideBar() {
-  // Complete aqui
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton(id: number): void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId,}: SideBarProps){
+  return(
+    <nav className="sidebar"> 
+      <span>
+        Watch <p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button 
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId == genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
